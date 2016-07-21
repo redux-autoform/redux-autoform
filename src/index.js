e5d3913fd00617cc2d1bb3a73d8e8738137143b5
Jonatan Salas <jonatansalas@live.com.ar>
2016-07-21 16:56:34 -0300

    Deleted factories from index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ export AutoForm from './AutoForm.js';
 
 // component factories
 export ComponentFactory from 'redux-autoform-utils/lib/factory/ComponentFactory';
-// export DefaultEditComponentFactory from './factory/bootstrap/BootstrapEditComponentFactory';
-// export DefaultDetailsComponentFactory from './factory/bootstrap/BootstrapDetailsComponentFactory';
 
 //localizers
 export momentLocalizer from 'redux-autoform-utils/lib/localization/momentLocalizer';
