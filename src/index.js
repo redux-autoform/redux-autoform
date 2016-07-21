34e144bafa0af123a088843d1e9668f011aa3fca
Jonatan Salas <jonatansalas@live.com.ar>
2016-07-21 15:03:17 -0300

    Default factories will not be exposed anymore by the index.js since they will get it's own packages.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 // main component
 export AutoForm from './AutoForm.js';
 
-
-//TODO JS: add redux-autoform-utils import
 // component factories
 export ComponentFactory from 'redux-autoform-utils/lib/factory/ComponentFactory';
-export DefaultEditComponentFactory from './factory/bootstrap/BootstrapEditComponentFactory';
-export DefaultDetailsComponentFactory from './factory/bootstrap/BootstrapDetailsComponentFactory';
+// export DefaultEditComponentFactory from './factory/bootstrap/BootstrapEditComponentFactory';
+// export DefaultDetailsComponentFactory from './factory/bootstrap/BootstrapDetailsComponentFactory';
 
 //localizers
 export momentLocalizer from 'redux-autoform-utils/lib/localization/momentLocalizer';
