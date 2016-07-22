1973f109ba5ae1a81913f369d9176154291d2a48
Jonatan Salas <jonatansalas@live.com.ar>
2016-07-21 21:19:56 -0300

    - Trying to update travis.yml, karma and package.json to add codecov support

--- karma.conf.js
+++ karma.conf.js
@@ -28,6 +28,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/**/*.js': ['coverage'],
       'test/index.js': ['webpack', 'sourcemap']
     },
 
@@ -41,12 +42,14 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
+    coverageReporter: {
+      reporters: [{type: 'lcov'}]
+    },
 
     // web server port
     port: 9876,
-
 
     // enable / disable colors in the output (reporters and logs)
     colors: true,
