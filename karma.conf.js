7d9b8c682fd3f3ce5c3ddd3f34b2601f362d5f5c
Jonatan Salas <jonatansalas@live.com.ar>
2016-07-21 22:43:43 -0300

    Revert changes over codecov

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,6 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/**/*.js': ['coverage'],
       'test/index.js': ['webpack', 'sourcemap']
     },
 
@@ -42,11 +41,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
-
-    coverageReporter: {
-      reporters: [{type: 'lcov'}]
-    },
+    reporters: ['progress'],
 
     // web server port
     port: 9876,
