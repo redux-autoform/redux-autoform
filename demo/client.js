5e7b45ba2763c2778e2e4accbad64360bb26f213
André Pena <andrerpena@gmail.com>
2016-08-01 20:38:56 -0300

    Adding DevTools to the demo #61

--- demo/client.js
+++ demo/client.js
@@ -2,13 +2,18 @@ import React from 'react';
 import {render} from 'react-dom';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
+import DevTools from './components/DevTools';
+
 
 const store = configureStore();
 
 
 render(
     <Provider store={store}>
-        <div>Redux should be working</div>
+        <div>
+            Redux should be working
+            <DevTools />
+        </div>
     </Provider>,
     document.getElementById('#app_container')
 );
