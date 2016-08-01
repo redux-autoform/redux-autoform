922bdbb70197b32a3651aa21793edfaeadb6a09a
André Pena <andrerpena@gmail.com>
2016-08-01 20:37:14 -0300

    Redux now working on the demo #61

--- demo/client.js
+++ demo/client.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+
+const store = configureStore();
+
+
 render(
-    <div>Working</div>,
+    <Provider store={store}>
+        <div>Redux should be working</div>
+    </Provider>,
     document.getElementById('#app_container')
 );
